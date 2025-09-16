4080fc773a24d5ad6786b23e772fbdc217d50689
jeremmy <elcheremmy@gmail.com>
2025-09-16 16:26:23 -0500

    Actualizo index.php con el post de categorias

--- index.php
+++ index.php
@@ -42,4 +42,47 @@ elseif ($opcion == "0") {
     }
 }
 
+//metodo post//
+$respuesta = readline("¿Deseas agregar una nueva categoría? escoge 1 para si o  0 para no: ");
+
+    if($respuesta === "1") {
+        $Nombre = readline(" Escribe el nombre de la categoría: ");
+        $Descripcion = readline(" Escribe la descripción de la categoría: ");
+    
+        $data = array(
+            'nombreCategoria' => $Nombre,
+            'descripcion' => $Descripcion
+        );
+        $data_categorias = json_encode($data);    
+
+        $proceso = curl_init($urlCategorias);
+
+
+        curl_setopt($proceso, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($proceso, CURLOPT_POSTFIELDS, $data_categorias);    
+        curl_setopt($proceso, CURLOPT_RETURNTRANSFER, true);
+
+        curl_setopt($proceso, CURLOPT_HTTPHEADER, array(                                    
+            'Content-Type: application/json',                                          
+            'Content-Length: ' . strlen($data_categorias))                                 
+        );
+        
+        $respuestapet = curl_exec($proceso);
+
+        $http_code = curl_getinfo($proceso, CURLINFO_HTTP_CODE);
+
+        if (curl_errno($proceso)){
+
+            die('Error en la petición post : ' . curl_error($proceso) . "\n");
+        }
+
+        curl_close($proceso);
+
+        if ($http_code === 200) {
+            echo "Categoría agregada exitosamente.\n";
+        } else {
+            echo "Error al agregar la categoría. ERROR: $http_code\n";
+        }
+    }
 ?>
+
